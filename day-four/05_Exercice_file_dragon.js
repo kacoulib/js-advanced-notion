56d6bbd4dd4d2ea16009fe01462373154ee18883
kacoulib <kacoulib@gmail.com>
2022-09-22 23:58:34 +0200

    style: indent

--- day-four/05_Exercice_file_dragon.js
+++ day-four/05_Exercice_file_dragon.js
@@ -4,38 +4,37 @@ const filePromise = (url) =>
   new Promise((resolve, reject) => {
     fs.readFile(url, { encoding: "utf8" }, (err, data) => {
       if (err) {
-        reject(new Error("File read failed:"))
+        reject(new Error("File read failed:"));
         return;
       }
-     
+
       resolve(data);
     });
   });
 
-  filePromise('./data/dragons.json').then( data => {
-    const { dragons } =  JSON.parse(data);
+filePromise("./data/dragons.json").then((data) => {
+  const { dragons } = JSON.parse(data);
 
-    let oldDragon = { age : 0, name : null } ;
-    for(const dragon of dragons){
-        const { age, name  } = dragon;
-        if(age > oldDragon.age){
-            oldDragon.age = age;
-            oldDragon.name = name;
-        }
+  let oldDragon = { age: 0, name: null };
+  for (const dragon of dragons) {
+    const { age, name } = dragon;
+    if (age > oldDragon.age) {
+      oldDragon.age = age;
+      oldDragon.name = name;
     }
-    console.log(oldDragon);
+  }
+  console.log(oldDragon);
 
-    dragons.sort( (d1, d2) => d1.age - d2.age ) ;
+  dragons.sort((d1, d2) => d1.age - d2.age);
 
-    console.log(dragons.slice(-1));
-    const { age: ageY, name : nameY } = dragons.slice(-1).shift(); 
-    const { age: ageO, name : nameO } = dragons.slice(0,1).shift(); 
+  console.log(dragons.slice(-1));
+  const { age: ageY, name: nameY } = dragons.slice(-1).shift();
+  const { age: ageO, name: nameO } = dragons.slice(0, 1).shift();
 
-    console.log(`Le plus jeune : ${ageY} age ${nameY}`)
-    console.log(`Le plus vieux : ${ageO} age ${nameO}`);
+  console.log(`Le plus jeune : ${ageY} age ${nameY}`);
+  console.log(`Le plus vieux : ${ageO} age ${nameO}`);
 
-    dragons.sort( (d1, d2) => d2.age - d1.age ) ;
+  dragons.sort((d1, d2) => d2.age - d1.age);
 
-    console.log(dragons)
-
-  });
+  console.log(dragons);
+});
